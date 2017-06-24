b69a87bc2aaec184afca68c35f2afb6375e58d5f
kai <1058495760@qq.com>
2017-06-24 11:51:47 +0800

    添加常量类，添加图表的几种显示颜色值

---
+++ src/com/littletool/Constant.java
@@ -0,0 +1,10 @@
+package com.littletool;
+
+public class Constant {
+	
+	public static final String COLOR_POSITIVE = "006400";
+	public static final String COLOR_NEGATIVE = "ff0000";
+	public static final String COLOR_SIGNAL_END = "ffff00";
+	public static final String COLOR_PREPARE_ENTER = "ed7daN";
+	
+}
